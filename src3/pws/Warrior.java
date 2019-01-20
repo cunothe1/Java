a9b1472bb6b8ec558f374bcf458d387bdb82cd38
cuno <cunothe1@users.noreply.github.com>
2019-01-20 14:28:05 +0100

    Add files via upload

---
+++ src3/pws/Warrior.java
@@ -0,0 +1,7 @@
+package pws;
+
+public class Warrior extends Characters {
+	
+	public Warrior() {
+	}
+}
---
+++ src3/pws/empty.java
@@ -0,0 +1,14 @@
+package pws;
+
+public class empty extends Characters {
+
+	public empty() {
+		setInfo(0,0);
+		setInfo(1,1);
+		setInfo(3,1);
+		setInfo(4,1);
+		setInfo(5,1);
+		setInfo(6,1);
+		setInfo(7,1);
+	}
+}
